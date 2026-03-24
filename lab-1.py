d753bc62b82c88a224734e33b65f034ece97ef8c
KorobovaKate <kat3kor@yandex.ru>
2026-03-24 18:39:17 +0300

    Update lab-1.py

--- lab-1.py
+++ lab-1.py
@@ -1,24 +1,21 @@
-# Извлечение букв из строки, содержащей числа и буквы.
+#Проверка, является ли символ буквой
+def is_letter(char):
+    return char.isalpha()
+
 def main():
 
     while True:
         
-        # Ввод данных с клавиатуры
         user_input = input("Введите строку (числа и буквы): ")
 
-        # Проверка на пустой ввод
         if not user_input:
             print("Ошибка: введена пустая строка. Попробуйте снова.")
             continue
-
-        # Проверка, является ли символ буквой
-        is_letter = lambda char: char.isalpha()
-
-        # Фильтрация строки 
-        filter_chars = filter(is_letter, user_input)
-
-        # Преобразование фильтра в список и соединение в строку
-        result_string = "".join(list(filter_chars))
+        
+        filtered_chars = list(filter(is_letter, user_input))
+        
+        # Преобразование списка символов в единую строку
+        result_string = "".join( filtered_chars)
 
         if not result_string:
             print("Ошибка: В строке нет букв. Попробуйте снова.")
@@ -31,6 +28,5 @@ def main():
     print(f"Извлеченные буквы: {result_string}")
     print("-" * 30)
 
-# Точка входа
 if __name__ == "__main__":
     main()
